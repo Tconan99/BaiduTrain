1aab5a62aa3f646a2f09d6bcb947251f51d4aa89
xueep <xueep@jcinfo.online>
2019-08-22 18:18:40 +0800

    提交比较靠谱的模型与代码

--- check.py
+++ check.py
@@ -4,7 +4,7 @@ from snownlp import sentiment
 import numpy as np
 import os
 
-sentiment.classifier.load('target/sen 20190822 133553.marshal')
+sentiment.classifier.load('target/sen 20190822 165752.marshal')
 
 def run(isTrain=True):
     rows = []
@@ -21,53 +21,54 @@ def run(isTrain=True):
     filename_answer = 'target/answer.csv'
     if os.path.exists(filename_answer): 
         os.remove(filename_answer)
-    with open(filename_answer, 'a+', encoding='gbk') as csv_answer:
-        writer = csv.writer(csv_answer)
+    
         
-        wrong = 0
-        total = len(rows)
-        progress = 0
-        for row in rows[0:]:
+    wrong = 0
+    total = len(rows)
+    progress = 0
+    for row in rows[0:]:
 
-            progress += 1
+        progress += 1
 
-            id = row[0]
-            content = row[2]
-            answer = ''
-            if isTrain:
-                answer = row[3]
+        id = row[0]
+        content = row[2]
+        answer = ''
+        if isTrain:
+            answer = row[3]
 
-            avg = 1
-            if content and content != '':
-                s = SnowNLP(content)
-                # avg = np.mean(s.sentences)
-                avg = s.sentiments
+        avg = 1
+        if content and content != '':
+            s = SnowNLP(content)
+            # avg = np.mean(s.sentences)
+            avg = s.sentiments
 
-            if progress % 50 == 0:
-                print("progress=" + str(progress) + "," + str(100.0 * progress / total) + "%, wrong: " + str(wrong) + " -> " + str(100.0 * wrong / (progress)) + "%")
+        if progress % 50 == 0:
+            print("progress=" + str(progress) + "," + str(100.0 * progress / total) + "%, wrong: " + str(wrong) + " -> " + str(100.0 * wrong / (progress)) + "%")
 
-            if False :
-                print(row)
-                # print(content)
-                print(avg)
+        if False :
+            print(row)
+            # print(content)
+            print(avg)
 
-                if progress % 1 == 0:
-                    input()
+            if progress % 1 == 0:
+                input()
 
-            result = 1
-            if avg < 0.2:
-                result = 0
-            elif avg > 0.8:
-                result = 2
-            
-            if isTrain and result != int(answer):
-                wrong += 1
+        result = 1
+        if avg < 0.2:
+            result = 0
+        elif avg > 0.8:
+            result = 2
+        
+        if isTrain and result != int(answer):
+            wrong += 1
 
-            if not isTrain:
-                list.append([id, result])
-                if len(list) > 100:
-                    writer.writerows(list)
-                    list.clear()
-                
+        if not isTrain:
+            list.append([id, result])
+
+    if len(list) > 0:
+        with open(filename_answer, 'w+', encoding='gbk') as csv_answer:
+            writer = csv.writer(csv_answer)
+            writer.writerows(list)
+        list.clear()            
 # run(isTrain=True)
 run(isTrain=False)
--- format_data.py
+++ format_data.py
@@ -31,16 +31,34 @@ def format():
             elif int(answer) == 2:
                 pos.append(content)
 
-      
         with open('target/pos.txt', 'w+') as f_pos:
-            f_pos.write('\n'.join(pos))
-
+            f_pos.write('\n'.join(pos[:]))
+        
         with open('target/neg.txt', 'w+') as f_neg:
-            f_neg.write('\n'.join(neg))
+            f_neg.write('\n'.join(neg[:]))
+
+printTime = lambda t: "%02d:%02d:%02d" % (int(t/60/60)%60, int(t/60)%60, int(t)%60)
+start = 0
+def printPer(index, total, type):
+
+    if index % 100 != 0:
+        return
+
+    pass_time = time.time() - start
+    per = index * 100.0 / total
+
+    total_time = 999999
+    if index > 0:
+        total_time = int(pass_time / index * total)
+    
+    print("%d: %.2f%% -> %s/%s" % (type, per, printTime(pass_time), printTime(total_time)))
 
 def train():
     time_str = (datetime.datetime.now().strftime('%Y%m%d %H%M%S'))   #日期格式化
-    sentiment.train('target/neg.txt', 'target/pos.txt')
+    print("prepare...")
+    global start
+    start = time.time()
+    sentiment.train('target/neg.txt', 'target/pos.txt', printPer)
     sentiment.save('target/sen %s.marshal' % time_str)
 
 format()
